b781fff4ee16f714a353504f89064d72e1c958bc
Andrey Kozlov <pilotc@bk.ru>
2020-04-19 22:39:10 +0600

    solution

---
+++ src/ru/geekbrains/home_level2_lesson2/CalculateArrayThred.java
@@ -0,0 +1,24 @@
+package ru.geekbrains.home_level2_lesson2;
+
+public class CalculateArrayThred extends Thread {
+    private long timeRun;
+    private float calcArr [];
+
+    public CalculateArrayThred(long timeRun, float [] calcArr) {
+        this.timeRun = timeRun;
+        this.calcArr = calcArr;
+    }
+
+    public long getTime () {
+        return timeRun;
+    }
+
+    @Override
+    public void run() {
+        long timeBegin = System.currentTimeMillis();
+        for (int i =0; i < calcArr.length; i++)
+         calcArr[i] = (float) (calcArr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        timeRun = System.currentTimeMillis()-timeBegin;
+        interrupt();
+    }
+}
---
+++ src/ru/geekbrains/home_level2_lesson2/Main.java
@@ -0,0 +1,117 @@
+package ru.geekbrains.home_level2_lesson2;
+
+public class Main {
+    static final int size = 10_000_000;
+    static final int h = size / 2;
+
+    // Метод заполняет массив "1" или по формуле. Возвращает время заполнения в миллисекундах
+    private static long fillArrayNumber (int size, float [] arr, int flag) {
+        long startTime, endTime;
+        if (flag == 1) {
+            startTime = System.currentTimeMillis();
+            for (int i = 0; i < size; i++) arr[i] = 1;
+            endTime = System.currentTimeMillis();
+        }
+          else {
+            startTime = System.currentTimeMillis();
+            for (int i = 0; i < size; i++)
+                arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            endTime = System.currentTimeMillis();
+        }
+
+        return endTime-startTime;
+    }
+
+    // Метод разделяет исходный массив на 2 массива. Возвращает время заполнения в миллисекундах
+    private static long divadedArray(float[] arr, float[] a1, float[] a2) {
+        long startTime = System.currentTimeMillis();
+        System.arraycopy(arr, 0, a1, 0, Main.h);
+        System.arraycopy(arr, Main.h, a2, 0, Main.h);
+        return System.currentTimeMillis() - startTime;
+    }
+
+    // Метод склеивает исходные 2 массива в 1 массив. Возвращает время заполнения в миллисекундах
+    private static long stikArray (float[] a1, float [] a2, float [] arr) {
+        long startTime = System.currentTimeMillis();
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+
+        float[] arr = new float[size];
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+        long startTime;
+
+        // Заполняем  массив "1" выводим время
+        System.out.println("Время заполнения массива еденицей (msec) " + fillArrayNumber(size,arr,1));
+
+        // Вычисляем эдементы массива, выводим время
+        System.out.println("Время вычисления массива простой метод (msec) " + fillArrayNumber(size,arr,2));
+
+        // Заполняем  массив "1" выводим время
+       fillArrayNumber(size,arr,1);
+
+        Thread first = new Thread() {
+            @Override
+            public void run() {
+                for (int i =0; i < h; i++)
+                    a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                interrupt();
+            }
+        };
+        Thread second = new Thread() {
+            @Override
+            public void run() {
+                for (int i =0; i < h; i++) {
+                    a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+                interrupt();
+            }
+        };
+
+        // Разбиваем массив, выводим время
+        System.out.println("Время разбивки массива (msec) " + divadedArray(arr,a1,a2));
+
+        // Запускаем потоки
+        startTime = System.currentTimeMillis();
+        first.start();
+        second.start();
+        first.interrupt();
+        second.interrupt();
+
+        // Здесь могла быть Ваша реклама :)))
+
+        while (first.isInterrupted() & second.isInterrupted()) {System.nanoTime();}
+
+        System.out.println("Время вычисления массива потоками (msec) " + (System.currentTimeMillis()-startTime));
+        System.out.println("Время склейки массива потоком (msec) " + (stikArray(a1,a2,arr)));
+
+
+        System.out.println("Проверка элементов массива " + arr[0] +" " +arr[1] + " "+ arr[9_999_998] + " " +arr[9_999_999]);
+
+        //  Решение через отдельный класс
+
+        // Заполняем 1 и разбиваем на 2 массива
+        fillArrayNumber(size,arr,1);
+        divadedArray(arr,a1,a2);
+
+        CalculateArrayThred t1 = new CalculateArrayThred(0,a1);
+        CalculateArrayThred t2 = new CalculateArrayThred(0,a2);
+        t1.start();
+        t2.start();
+        t1.interrupt();
+        t2.interrupt();
+        while (t1.isInterrupted() & t2.isInterrupted()) {System.nanoTime();}
+
+        // Склеиваем массивы
+        stikArray(a1,a2,arr);
+        System.out.println("Время расчета потоком 1 " + t1.getTime());
+        System.out.println("Время расчета потоком 2 " + t2.getTime());
+
+        System.out.println("Проверка элементов массива " + arr[0] +" " +arr[1] + " "+ arr[9_999_998] + " " +arr[9_999_999]);
+
+    }
+}
